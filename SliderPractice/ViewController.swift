6020b1dddad4de37bc874a2aad58765d064aaf6a
Bogdan Anishchenkov <bogdaggi@gmail.com>
2022-08-05 09:41:46 +0300

    Initial Commit

---
+++ SliderPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderPractice
+//
+//  Created by Богдан Анищенков on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
